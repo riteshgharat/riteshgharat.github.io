5de6c1e2d8cdc2207b72abcab21e4235163da91e
riteshgharat05@gmail.com <riteshgharat05@gmail.com>
2022-11-07 11:50:47 +0530

    updated

--- math-html/math.js
+++ math-html/math.js
@@ -13,17 +13,15 @@ function evaluate(value) {
   return a;
 }
 console.info('math.html 1.0.0 https://riteshgharat.github.io/math-html');
-window.onload = function() {
-  document.querySelectorAll('math').forEach(mTag => {
-    let mTagValue = mTag.innerHTML.split(' ');
-    for (var i = 3; i < (mTagValue.length - 2); i++) {
-      for (var j = 0; j < mathMethod.length; j++) {
-        if (mTagValue[i].toString().search(mathMethod[j]) !== -1) mTagValue[i] = evaluate(mTagValue[i]);
-      }
+document.querySelectorAll('math').forEach(mTag => {
+  let mTagValue = mTag.innerHTML.split(' ');
+  for (var i = 3; i < (mTagValue.length - 2); i++) {
+    for (var j = 0; j < mathMethod.length; j++) {
+      if (mTagValue[i].toString().search(mathMethod[j]) !== -1) mTagValue[i] = evaluate(mTagValue[i]);
     }
-    for (var i = 3; i < mTagValue.length - 2; i++) {
-      mTagValue[i] = eval(mTagValue[i]);
-    }
-    mTag.innerHTML = mTagValue.toString().replace(/,/g, ' ');;
-  });
-}
+  }
+  for (var i = 3; i < mTagValue.length - 2; i++) {
+    mTagValue[i] = eval(mTagValue[i]);
+  }
+  mTag.innerHTML = mTagValue.toString().replace(/,/g, ' ');;
+});
