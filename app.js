898e3ddf228cbd058ee9775141a385d48c81e54b
riteshgharat05@gmail.com <riteshgharat05@gmail.com>
2023-02-22 22:45:48 +0530

    updated

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ window.onload = function() {
 // chaning windows on hash change
 window.addEventListener('hashchange', () => {
   let hash = (location.hash).replace('#', '');
-  document.querySelectorAll('.link').forEach(l => { l.classList.remove('active') });
-  document.querySelectorAll('section').forEach(s => { s.classList.remove('active') });
-  document.querySelector(`.${hash}-section`).classList.add('active');
-  document.querySelector(`.${hash}`).classList.add('active');
-})
+  if (hash == 'home' || hash == 'project' || hash == 'about' || hash == 'contact') {
+    document.querySelectorAll('.link').forEach(l => { l.classList.remove('active') });
+    document.querySelectorAll('section').forEach(s => { s.classList.remove('active') });
+    document.querySelector(`.${hash}-section`).classList.add('active');
+    document.querySelector(`.${hash}`).classList.add('active');
+  } else {
+    document.querySelectorAll('section').forEach(s => { s.classList.remove('active') });
+    document.querySelector(`.error-section`).classList.add('active');
+    error404();
+  }
+});
 
 // for updating projects card
 const projectData = [
@@ -35,6 +41,13 @@ const projectData = [
     img: "/assets/sdsmmap.webp",
     repo: "https://github.com/riteshgharat/sdsm-college-map",
     live: "https://sdsmcollegemap.netlify.app"
+  },
+  {
+    name: "devresources",
+    description: "Learn, Earn & Develop",
+    img: "/assets/devresources.webp",
+    repo: "#",
+    live: "https://devresources.tech"
   },
   {
     name: "What's AI?",
@@ -68,4 +81,24 @@ for (let i = 0; i < projectData.length; i++) {
   </div>
 </div>
 `;
+}
+
+const jokes = [
+  "Page not found. Looks like someone forgot to pay the internet bill.",
+  "Oops, it looks like we misplaced that page. Maybe check under the couch cushions?",
+  "This page is as elusive as a unicorn. Keep searching!",
+  "The page you're looking for seems to have taken a long vacation. Please try again later.",
+  "We're sorry, the internet is on vacation. Please come back another time.",
+  "Looks like you've hit a dead end. Time to turn around and try a different route!",
+  "Oh no, the page you're looking for is playing hide and seek. Try searching for it again!",
+  "We apologize for the inconvenience, but the page you're looking for is currently out to lunch.",
+  "This page is MIA. Maybe it's hiding in the Bermuda Triangle?",
+  "The page you're looking for seems to have vanished into thin air. Don't worry, we're on the case!"
+];
+
+/*Generating random message for error*/
+function error404() {
+  let no = Math.floor(Math.random() * 10)
+  let errorMsg = document.querySelector('.message');
+  errorMsg.innerText = jokes[no];
 }
