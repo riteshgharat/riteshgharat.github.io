6db5ac01e24836fcba68d5a81097815fb092bed2
riteshgharat <riteshgharat05@gmail.com>
2026-05-27 23:20:53 +0530

    fix: update base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/riteshgharat.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
